50dc95404873ed1b9faaee096211b5a814a04397
Seger01 <sjw.sars@student.avans.nl>
2024-04-25 22:53:37 +0200

    small change

--- WK1/3.8/.Module.symvers.cmd
+++ WK1/3.8/.Module.symvers.cmd
@@ -1,1 +1,1 @@
-cmd_/home/seger/driverontwikkeling/3.8/Module.symvers := sed 's/\.ko$$/\.o/' /home/seger/driverontwikkeling/3.8/modules.order | scripts/mod/modpost -m -a  -o /home/seger/driverontwikkeling/3.8/Module.symvers -e -i Module.symvers   -T -
+cmd_/home/seger/driverontwikkeling/WK1/3.8/Module.symvers := sed 's/\.ko$$/\.o/' /home/seger/driverontwikkeling/WK1/3.8/modules.order | scripts/mod/modpost -m -a  -o /home/seger/driverontwikkeling/WK1/3.8/Module.symvers -e -i Module.symvers   -T -
